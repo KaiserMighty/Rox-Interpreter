4ab79294438fc809c6ca93bafda433f0a4c7dbd9
Kaiser Mighty <unikmey@gmail.com>
2025-01-21 14:30:58 -0800

    Global Variables

--- compiler/table.c
+++ compiler/table.c
@@ -127,10 +127,10 @@ ObjString* tableFindString(Table* table, const char* chars, int length, uint32_t
         {
             if (IS_NIL(entry->value)) return NULL;
         }
-        else if (entry->key->length == length && entry->key->hash == hash && memcpy(entry->key->chars, chars, length) == 0)
+        else if (entry->key->length == length && entry->key->hash == hash && memcmp(entry->key->chars, chars, length) == 0)
         {
             return entry->key;
         }
-        index = (index + 1) %  table->capacity;
+        index = (index + 1) % table->capacity;
     }
 }
