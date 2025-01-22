9ab4b8206ba0fe49aa9f39dd1ea3a32691562abc
Kaiser Mighty <unikmey@gmail.com>
2025-01-22 14:52:49 -0800

    NaN Boxing and Hash Table Probing Optimizations

--- compiler/table.c
+++ compiler/table.c
@@ -11,19 +11,19 @@
 void initTable(Table* table)
 {
     table->count = 0;
-    table->capacity = 0;
+    table->capacity = -1;
     table->entries = NULL;
 }
 
 void freeTable(Table* table)
 {
-    FREE_ARRAY(Entry, table->entries, table->capacity);
+    FREE_ARRAY(Entry, table->entries, table->capacity + 1);
     initTable(table);
 }
 
 static Entry* findEntry(Entry* entries, int capacity, ObjString* key)
 {
-    uint32_t index = key->hash % capacity;
+    uint32_t index = key->hash & capacity;
     Entry* tombstone = NULL;
     for (;;)
     {
@@ -43,7 +43,7 @@ static Entry* findEntry(Entry* entries, int capacity, ObjString* key)
         {
             return entry;
         }
-        index = (index + 1) % capacity;
+        index = (index + 1) & capacity;
     }
 }
 
@@ -58,14 +58,14 @@ bool tableGet(Table* table, ObjString* key, Value* value)
 
 static void adjustCapacity(Table* table, int capacity)
 {
-    Entry* entries = ALLOCATE(Entry, capacity);
-    for (int i = 0; i < capacity; i++)
+    Entry* entries = ALLOCATE(Entry, capacity + 1);
+    for (int i = 0; i <= capacity; i++)
     {
         entries[i].key = NULL;
         entries[i].value = NIL_VAL;
     }
     table->count = 0;
-    for (int i = 0; i < table->capacity; i++)
+    for (int i = 0; i <= table->capacity; i++)
     {
         Entry* entry = &table->entries[i];
         if (entry->key == NULL) continue;
@@ -74,16 +74,16 @@ static void adjustCapacity(Table* table, int capacity)
         dest->value = entry->value;
         table->count++;
     }
-    FREE_ARRAY(Entry, table->entries, table->capacity);
+    FREE_ARRAY(Entry, table->entries, table->capacity + 1);
     table->entries = entries;
     table->capacity = capacity;
 }
 
 bool tableSet(Table* table, ObjString* key, Value value)
 {
-    if (table->count + 1 > table->capacity * TABLE_MAX_LOAD)
+    if (table->count + 1 > (table->capacity + 1) * TABLE_MAX_LOAD)
     {
-        int capacity = GROW_CAPACITY(table->capacity);
+        int capacity = GROW_CAPACITY(table->capacity + 1) - 1;
         adjustCapacity(table, capacity);
     }
     Entry* entry = findEntry(table->entries, table->capacity, key);
@@ -106,7 +106,7 @@ bool tableDelete(Table* table, ObjString* key)
 
 void tableAddAll(Table* from, Table* to)
 {
-    for (int i = 0; i < from->capacity; i++)
+    for (int i = 0; i <= from->capacity; i++)
     {
         Entry* entry = &from->entries[i];
         if (entry->key != NULL)
@@ -119,7 +119,7 @@ void tableAddAll(Table* from, Table* to)
 ObjString* tableFindString(Table* table, const char* chars, int length, uint32_t hash)
 {
     if (table->count == 0) return NULL;
-    uint32_t index = hash % table->capacity;
+    uint32_t index = hash & table->capacity;
     for (;;)
     {
         Entry* entry = &table->entries[index];
@@ -131,13 +131,13 @@ ObjString* tableFindString(Table* table, const char* chars, int length, uint32_t
         {
             return entry->key;
         }
-        index = (index + 1) % table->capacity;
+        index = (index + 1) & table->capacity;
     }
 }
 
 void tableRemoveWhite(Table* table)
 {
-    for (int i = 0; i < table->capacity; i++)
+    for (int i = 0; i <= table->capacity; i++)
     {
         Entry* entry = &table->entries[i];
         if (entry->key != NULL && !entry->key->obj.isMarked)
@@ -149,7 +149,7 @@ void tableRemoveWhite(Table* table)
 
 void markTable(Table* table)
 {
-    for (int i = 0; i < table->capacity; i++)
+    for (int i = 0; i <= table->capacity; i++)
     {
         Entry* entry = &table->entries[i];
         markObject((Obj*)entry->key);
